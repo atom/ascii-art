7e287a45af38c953ea45780decd9c496c74aa27e
ungb <ungb@github.com>
2017-10-18 13:31:27 -0700

    ran standard --fix

--- spec/ascii-art-spec.js
+++ spec/ascii-art-spec.js
@@ -1,32 +1,32 @@
-const AsciiArt = require('../lib/ascii-art');
+const AsciiArt = require('../lib/ascii-art')
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe("AsciiArt", function() {
-  let [workspaceElement, activationPromise] = Array.from([]);
+describe('AsciiArt', function () {
+  let [workspaceElement, activationPromise] = Array.from([])
 
-  beforeEach(function() {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('ascii-art');
+  beforeEach(function () {
+    workspaceElement = atom.views.getView(atom.workspace)
+    activationPromise = atom.packages.activatePackage('ascii-art')
 
-    return waitsForPromise(() => atom.workspace.open());
-  });
+    return waitsForPromise(() => atom.workspace.open())
+  })
 
-  return it("converts", function() {
-    const editor = atom.workspace.getActiveTextEditor();
-    editor.insertText("cool");
-    editor.selectAll();
-    const changeHandler = jasmine.createSpy('changeHandler');
-    editor.onDidChange(changeHandler);
+  return it('converts', function () {
+    const editor = atom.workspace.getActiveTextEditor()
+    editor.insertText('cool')
+    editor.selectAll()
+    const changeHandler = jasmine.createSpy('changeHandler')
+    editor.onDidChange(changeHandler)
 
-    atom.commands.dispatch(workspaceElement, 'ascii-art:convert');
+    atom.commands.dispatch(workspaceElement, 'ascii-art:convert')
 
-    waitsForPromise(() => activationPromise);
+    waitsForPromise(() => activationPromise)
 
-    waitsFor(() => changeHandler.callCount > 0);
+    waitsFor(() => changeHandler.callCount > 0)
 
     return runs(() =>
       expect(editor.getText()).toEqual(`\
@@ -40,6 +40,6 @@ describe("AsciiArt", function() {
 \
 `
       )
-    );
-  });
-});
+    )
+  })
+})
